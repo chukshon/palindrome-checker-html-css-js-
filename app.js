e7aa1b4a77663785acc2fd48fcbee320c8b6a185
ChukwukaHonour <ChukwukaHonour@gmail.com>
2022-03-15 22:09:45 +0100

    Added javascript functionality

---
+++ app.js
@@ -0,0 +1,26 @@
+const button = document.querySelector("#btn");
+const input  = document.querySelector("#input");
+const info = document.querySelector(".info-txt");
+let txtInput;
+
+button.addEventListener("click", () => {
+    info.style.display = "block";
+    let reverseInput = txtInput.split("").reverse().join("");
+    if(txtInput !== reverseInput){
+        info.innerHTML = `No, <span>'${input.value}'</span> isn't a palindrome!`
+    }
+    else{
+        info.innerHTML = `Yes, <span>'${input.value}'</span> is a palindrome!`
+    }
+})
+
+input.addEventListener("keyup", function(){
+
+    txtInput = input.value.toLowerCase().replace(/[^A-Z0-9]/ig, "");
+    if(txtInput.value){
+        button.classList.add("active")
+    }else{
+        button.classList.remove("active")
+        info.style.display = "none";
+    }
+})
